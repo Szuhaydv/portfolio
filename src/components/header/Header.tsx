39fe2cbfd318ce444e06fd7c0b8bd34d95f5dadb
Szuhay Dávid <szuhaydv@gmail.com>
2024-01-21 03:42:47 +0100

    hero section

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,20 +1,46 @@
+import { useRef } from 'react';
 import './header.css'
 
 const Header = () => {
+  const hamburgerNav = useRef<any>(null)
+  const handleHamburgerClick = () => {
+    if (hamburgerNav.current != null) {
+      hamburgerNav.current.classList.toggle("showHamburgerMenu")
+    }
+  }
   return (
     <header>
-      <a href="#home">
-        <h2>Home</h2>
-      </a>
-      <a className="scroll-padding-link" href="#skills">
-        <h2>Skills</h2>
-      </a>
-      <a className="scroll-padding-link-projects" href="#projects">
-        <h2>Projects</h2>
-      </a>
-      <a className="scroll-padding-link" href="#contact">
-        <h2>Contact</h2>
-      </a>
+      <nav className="header-links">
+        <a href="#home">
+          <h2>Home</h2>
+        </a>
+        <a href="#skills">
+          <h2>Skills</h2>
+        </a>
+        <a href="#projects">
+          <h2>Projects</h2>
+        </a>
+        <a href="#contact">
+          <h2>Contact</h2>
+        </a>
+      </nav>
+      <div className="hamburger" onClick={() => handleHamburgerClick()}>
+        <i className="bi bi-list h1"></i>
+      </div>
+      <nav ref={hamburgerNav} className='hamburger-nav'>
+        <a onClick={() => handleHamburgerClick()} href="#home">
+          <h2>Home</h2>
+        </a>
+        <a onClick={() => handleHamburgerClick()} href="#skills">
+          <h2>Skills</h2>
+        </a>
+        <a onClick={() => handleHamburgerClick()} href="#projects">
+          <h2>Projects</h2>
+        </a>
+        <a onClick={() => handleHamburgerClick()} href="#contact">
+          <h2>Contact</h2>
+        </a>
+      </nav>
     </header>
   )
 }
